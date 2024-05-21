91c196f6768db048a4ce628ee31ef7bc15e5c2fd
syab-syab <syabsyabsyabsyab@gmail.com>
2024-05-21 21:49:46 +0900

    とりあえずタイマーを動かせた

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const time: Date = new Date()
-  console.log(typeof time)
+  // 例によってUNIX時間の数値を取得する
+  // 1秒 = 1000ミリ秒
+
+  const d: Date = new Date()
+
+  const uni: number = d.getTime()
+
+  const [time, setTime] = useState<number>(uni)
+
+  const [timeStamp, setTimestamp] = useState<string>("")
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(time + 1000);
+      const realTime: string = new Date(time).toString()
+      setTimestamp(realTime)
+    }, 1000)
+    return () => clearInterval(id);
+  }, [time])
+
+
 
   return (
     <div className="App">
       <h1>世界禁欲時計-Patienceday Clock(Doomsday Clockのパクり)-テスト</h1>
-      {/* <h2>{new Date()}</h2> */}
+      <h2>time: { timeStamp }</h2>
     </div>
   );
 }
