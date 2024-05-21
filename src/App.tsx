635a5962d190b44d09d7954012bc8bd7467df0e1
syab-syab <syabsyabsyabsyab@gmail.com>
2024-05-21 17:11:15 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const time: Date = new Date()
+  console.log(typeof time)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>世界禁欲時計-Patienceday Clock(Doomsday Clockのパクり)-テスト</h1>
+      {/* <h2>{new Date()}</h2> */}
     </div>
   );
 }
